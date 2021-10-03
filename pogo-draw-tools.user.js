38d5cea0e3d2c28859c4dee3e35bc54849a38788
PC-QUARTO\Gabriel <gabrielbeckstencel@gmail.com>
2021-10-03 00:15:50 -0300

    fixed condition on draw tools checker

--- pogo-draw-tools.user.js
+++ pogo-draw-tools.user.js
@@ -32,7 +32,7 @@
         let onDTLoaded = (callback, interval = 250, maxWaitDuration = 10000) => {
             const pluginInterval = setInterval(() => {
                 // Do not load code, if Draw Tools Plugin isn't loaded
-                if(window.plugin.drawTools === undefined && window.plugin.drawTools.boot === undefined) return;
+                if(window.plugin.drawTools === undefined || window.plugin.drawTools.boot === undefined) return;
                 // If Object does exist, clear the interval, and run the callback function.
                 clearInterval(pluginInterval)
                 callback()
